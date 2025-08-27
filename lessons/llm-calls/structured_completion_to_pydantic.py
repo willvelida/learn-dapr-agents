71c032513acd1895ef2b29d9d5f0a67bd05d4195
Will Velida <willvelida@hotmail.co.uk>
2025-08-27 19:01:22 +1000

    Saving structured example
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

--- lessons/llm-calls/structured_completion_to_pydantic.py
+++ lessons/llm-calls/structured_completion_to_pydantic.py
@@ -4,15 +4,19 @@ from pydantic import BaseModel
 from dapr_agents import OpenAIChatClient
 from dapr_agents.types import UserMessage
 
+# Load environment variables
 load_dotenv()
 
+# Define our data set
 class Book(BaseModel):
     title: str
     author: str
     genre: str
 
+# Initialize the chat client
 llm = OpenAIChatClient()
 
+# Get our structured response
 response: Book = llm.generate(
     messages=[UserMessage("Give me a New York Times bestselling book")], response_format=Book
 )
